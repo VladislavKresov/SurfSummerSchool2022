d8fe3fc2efc412e2231442fe2c5fa785cb6b8178
Vlad Kresov <vladislavkresov@gmail.com>
2022-07-15 11:03:12 +0300

    Empty project

---
+++ app/src/main/java/com/example/surfschool/data/LoginDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.surfschool.data
+
+import com.example.surfschool.data.model.LoggedInUser
+import java.io.IOException
+
+class LoginDataSource {
+
+    fun login(username: String, password: String): Result<LoggedInUser> {
+        try {
+            // TODO: handle loggedInUser authentication
+            val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
+            return Result.Success(fakeUser)
+        } catch (e: Throwable) {
+            return Result.Error(IOException("Error logging in", e))
+        }
+    }
+
+    fun logout() {
+        // TODO: revoke authentication
+    }
+}
